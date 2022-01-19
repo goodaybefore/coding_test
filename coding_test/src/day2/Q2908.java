a1417959a059111f82df428fc951f228413533c1
goodaybefore <goodaybefore@gmail.com>
2022-01-20 01:24:04 +0900

    코테 : Q2908 상수

---
+++ coding_test/src/day2/Q2908.java
@@ -0,0 +1,45 @@
+package day2;
+
+import java.util.Scanner;
+//백준 단계별로 풀어보기 - 문자열 - 상수
+public class Q2908 {
+	/*
+	 * 상근이의 동생 상수는 수학을 정말 못한다. 상수는 숫자를 읽는데 문제가 있다. 이렇게 수학을 못하는 상수를 위해서 상근이는 수의 크기를 비교하는 문제를 내주었다.
+	 * 상근이는 세 자리 수 두 개를 칠판에 써주었다. 그 다음에 크기가 큰 수를 말해보라고 했다.
+	 * 상수는 수를 다른 사람과 다르게 거꾸로 읽는다. 예를 들어, 734와 893을 칠판에 적었다면, 상수는 이 수를 437과 398로 읽는다. 따라서, 상수는 두 수중 큰 수인 437을 큰 수라고 말할 것이다.
+	 * 두 수가 주어졌을 때, 상수의 대답을 출력하는 프로그램을 작성하시오.
+	 * 
+	 * 
+	 * 조건) 첫째 줄에 상근이가 칠판에 적은 두 수 A와 B가 주어진다. 두 수는 같지 않은 세 자리 수이며, 0이 포함되어 있지 않다.
+	 * */
+	
+	/* 
+	 * 예상 풀이 방법 : */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int[] arrA = IntArr(a);
+		int[] arrB = IntArr(b);
+		
+		int newA = ReverseNum(arrA);
+		int newB = ReverseNum(arrB);
+		
+		System.out.println(newA>newB?newA:newB);
+	}
+	public static int[] IntArr(int num) {
+		int arr[] = new int[3];
+		arr[0] = num % 10;
+		arr[1] = (num/10)%10;
+		arr[2] = num/100;
+//		System.out.println(Arrays.toString(arr));
+		return arr;
+	}
+	public static int ReverseNum(int[] arr) {
+		return (arr[0]*100) + (arr[1]*10) + arr[2];
+	}
+	
+
+}
+
