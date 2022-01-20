354837a7680082eea2a9fa5aade408968fc8e961
goodaybefore <goodaybefore@gmail.com>
2022-01-21 00:03:47 +0900

    코테 : 크로아티아 알파벳
    - 정답 코드 복습해야함

--- coding_test/src/day2/Q2941_FAIL.java
+++ coding_test/src/day2/Q2941_FAIL.java
@@ -3,7 +3,7 @@ package day2;
 import java.util.Scanner;
 
 //백준 단계별로 풀어보기 - 문자열 - 크로아티아 알파벳
-public class Q2941 {
+public class Q2941_FAIL {
 	/*
 	 * 예전에는 운영체제에서 크로아티아 알파벳을 입력할 수가 없었다. 따라서, 다음과 같이 크로아티아 알파벳을 변경해서 입력했다
 	 *크로아티아 알파벳	변경
