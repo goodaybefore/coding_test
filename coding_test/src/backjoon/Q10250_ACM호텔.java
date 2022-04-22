821340ccffe2b4cd1679f55055a2e8b77838bfa9
goodaybefore <goodaybefore@gmail.com>
2022-04-23 00:45:16 +0900

    Update Q10250_ACM호텔.java

--- coding_test/src/backjoon/Q10250_ACM호텔.java
+++ coding_test/src/backjoon/Q10250_ACM호텔.java
@@ -7,7 +7,7 @@ public class Q10250_ACM호텔 {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		String [] resArray = new String[T];//테스트케이스를 담을 배열
+		int [] resArray = new int[T];//테스트케이스를 담을 배열
 		
 		for(int i=0;i<T;i++) {
 			int hotelHeight = sc.nextInt();
@@ -18,20 +18,20 @@ public class Q10250_ACM호텔 {
 			int width = 1;
 			int cnt = 0;
 
-			height = N/hotelHeight;
+			height = N%hotelHeight;
+			width = N/hotelHeight;
+			if(N%hotelHeight != 0) width += 1;
+			else height = hotelHeight;
 			//System.out.println("N번째 손님이 묵을 방");
-			//System.out.println("height : "+height);
+			//System.out.println("height : "+height*100);
 			//System.out.println("width : "+width);
-			String widthStr = "";
-			if(width<10) widthStr = "0"+width;
+			resArray[i] = height*100+width;
 			
-			resArray[i] = height+widthStr;
 		}
 		
-		for(int i=0;i<resArray.length;i++) {
-			System.out.println(resArray[i]);
+		for(int tmp:resArray) {
+			System.out.println(tmp);
 		}
-		
 		
 		
 	}
