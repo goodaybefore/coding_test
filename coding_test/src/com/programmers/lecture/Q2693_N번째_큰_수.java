066bc7b6774b34f71a1f7e0ee34eb09d1ba0e9e0
goodaybefore <goodaybefore@gmail.com>
2022-06-15 23:38:47 +0900

    Create Q2693_N번째_큰_수.java

---
+++ coding_test/src/com/programmers/lecture/Q2693_N번째_큰_수.java
@@ -0,0 +1,34 @@
+package com.programmers.lecture;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Q2693_N번째_큰_수 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;		
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<T;i++) {
+			Integer arr[] = new Integer[10];
+			st = new StringTokenizer(br.readLine()," ");
+			for(int j=0;j<10;j++) {
+				
+				arr[j] = Integer.parseInt(st.nextToken());
+			}
+//			System.out.println(Arrays.toString(arr));
+			sb.append(findThirdMax(arr)).append("\n");
+		}
+		System.out.println(sb);
+	}
+	public static Integer findThirdMax(Integer[] arr) {
+		Arrays.sort(arr, Collections.reverseOrder());
+		return arr[2];
+	}
+
+}
